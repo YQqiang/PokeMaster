a090372daf7c162d73878b07856f443531887155
yuqiang <yuqiang@sungrowpower.com>
2019-12-17 17:35:26 +0800

    PokemonInfoPanel

---
+++ PokeMaster/List/BlurView.swift
@@ -0,0 +1,9 @@
+//
+//  BlurView.swift
+//  PokeMaster
+//
+//  Created by sungrow on 2019/12/17.
+//  Copyright © 2019 sungrow. All rights reserved.
+//
+
+import Foundation
---
+++ PokeMaster/List/PokemonInfoPanelAbilityList.swift
@@ -0,0 +1,9 @@
+//
+//  PokemonInfoPanelAbilityList.swift
+//  PokeMaster
+//
+//  Created by sungrow on 2019/12/17.
+//  Copyright © 2019 sungrow. All rights reserved.
+//
+
+import Foundation
