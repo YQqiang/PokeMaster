743cb4e358bfc8ad7daea153d1605e3632825945
yuqiang <1054572107@qq.com>
2019-12-18 14:13:42 +0800

    MainTab

---
+++ PokeMaster/MainTab.swift
@@ -0,0 +1,25 @@
+//
+//  MainTab.swift
+//  PokeMaster
+//
+//  Created by sungrow on 2019/12/18.
+//  Copyright © 2019 sungrow. All rights reserved.
+//
+
+import SwiftUI
+
+struct MainTab: View {
+    var body: some View {
+        TabView {
+            PokemonRootView().tabItem {
+                Image(systemName: "list.bullet.below.rectangle")
+                Text("列表")
+            }
+            SettingRootView().tabItem {
+                Image(systemName: "gear")
+                Text("设置")
+            }
+        }
+        .edgesIgnoringSafeArea(.top)
+    }
+}
