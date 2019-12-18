ab031184fb11348b368e3e1976ad049f443a9df2
yuqiang <1054572107@qq.com>
2019-12-18 14:00:32 +0800

    切换模糊效果

--- PokeMaster/ContentView.swift
+++ PokeMaster/ContentView.swift
@@ -10,8 +10,8 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-//        PokemonList()
-        SettingRootView()
+        PokemonList()
+//        SettingRootView()
     }
 }
 
