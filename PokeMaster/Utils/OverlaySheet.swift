cd8fe08db12dced9e6be0acc6807a18400115e06
yuqiang <1054572107@qq.com>
2019-12-30 09:06:03 +0800

    panelDraggingGesture

--- PokeMaster/Utils/OverlaySheet.swift
+++ PokeMaster/Utils/OverlaySheet.swift
@@ -13,9 +13,22 @@ struct OverlaySheet<Content: View>: View {
     private let isPresented: Binding<Bool>
     private let makeContent: () -> Content
     
+    @GestureState private var translation = CGPoint.zero
+    
     init(isPresented: Binding<Bool>, @ViewBuilder content:@escaping () -> Content) {
         self.isPresented = isPresented
         self.makeContent = content
+    }
+    
+    var panelDraggingGesture: some Gesture {
+        DragGesture()
+            .updating($translation) { (current, state, _) in
+                state.y = current.translation.height
+            }.onEnded { (state) in
+                if state.translation.height > 250 {
+                    self.isPresented.wrappedValue = false
+                }
+            }
     }
     
     var body: some View {
@@ -23,9 +36,12 @@ struct OverlaySheet<Content: View>: View {
             Spacer()
             makeContent()
         }
-        .offset(y: isPresented.wrappedValue ? 0 : UIScreen.main.bounds.height)
+        .offset(
+            y: (isPresented.wrappedValue ? 0 : UIScreen.main.bounds.height) + max(0, translation.y)
+            )
         .animation(.interpolatingSpring(stiffness: 70, damping: 12))
         .edgesIgnoringSafeArea(.bottom)
+        .gesture(panelDraggingGesture)
     }
 }
 
