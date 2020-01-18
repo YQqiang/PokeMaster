548388c80c23542d6783acece30178636f2dbc36
yuqiang <1054572107@qq.com>
2020-01-18 15:15:34 +0800

    pokemaster://showPanel?id=25

--- PokeMaster/SceneDelegate.swift
+++ PokeMaster/SceneDelegate.swift
@@ -13,6 +13,39 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     var window: UIWindow?
 
+    private func showMainTab(scene: UIScene, with store: Store) {
+        if let windowScene = scene as? UIWindowScene {
+            let window = UIWindow(windowScene: windowScene)
+            window.rootViewController = UIHostingController(rootView: MainTab().environmentObject(store))
+            self.window = window
+            window.makeKeyAndVisible()
+        }
+    }
+    
+    private func createStore(_ URLContexts: Set<UIOpenURLContext>) -> Store {
+        let store = Store()
+        guard let url = URLContexts.first?.url,
+        let components = URLComponents(url: url, resolvingAgainstBaseURL: false) else {
+            return store
+        }
+        
+        switch (components.scheme, components.host) {
+        case ("pokemaster", "showPanel"):
+            guard let idQuery = (components.queryItems?.first {
+                $0.name == "id"
+                }),
+                let idString = idQuery.value,
+                let id = Int(idString),
+                id >= 1 && id <= 30
+            else {
+                break
+            }
+            store.appState.pokemonList.selectionState = .init(expandingIndex: id, panelIndex: id, panelPresented: true)
+        default:
+            break
+        }
+        return store
+    }
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
@@ -27,12 +60,13 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         let contentView = ContentView().environment(\.managedObjectContext, context)
 
         // Use a UIHostingController as window root view controller.
-        if let windowScene = scene as? UIWindowScene {
-            let window = UIWindow(windowScene: windowScene)
-            window.rootViewController = UIHostingController(rootView: contentView)
-            self.window = window
-            window.makeKeyAndVisible()
-        }
+        let store = createStore(connectionOptions.urlContexts)
+        showMainTab(scene: scene, with: store)
+    }
+    
+    func scene(_ scene: UIScene, openURLContexts URLContexts: Set<UIOpenURLContext>) {
+        let store = createStore(URLContexts)
+        showMainTab(scene: scene, with: store)
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
